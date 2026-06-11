b0444f89f1d3ad65148e2b42d7873c8e401b31f7
Ivan66lit <popov.1507i@gmail.com>
2026-06-11 20:32:10 +0200

    Bogo Sort bonus

--- BogoSort/BogoSort/Program.cs
+++ BogoSort/BogoSort/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> seznam = new List<int> { 90, -23, -7, 78 };
+            seznam = BogoSort(seznam);
+            for (int i = 0; i < seznam.Count; i++)
+                Console.WriteLine(seznam[i]);
+        }
+
+        public static List<int> BogoSort(List<int> seznam)
+        {
+            int a = 0;
+
+            for (int i = 1; i < seznam.Count; i++)
+            {
+                if (seznam[i] < seznam[i - 1])
+                {
+                    i = 0;
+                    a++;
+                    seznam = seznam.Shuffle().ToList();
+                }
+            }
+
+            Console.WriteLine(a);
+            Console.WriteLine("seznam je");
+            return seznam;
         }
     }
 }
