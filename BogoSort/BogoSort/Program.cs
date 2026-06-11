5124a188835c3ff0375459492096a3426d4fd425
Ivan66lit <popov.1507i@gmail.com>
2026-06-11 20:45:37 +0200

    Revert "Bogo Sort bonus"
    This reverts commit b0444f89f1d3ad65148e2b42d7873c8e401b31f7.

--- BogoSort/BogoSort/Program.cs
+++ BogoSort/BogoSort/Program.cs
@@ -4,29 +4,7 @@
     {
         static void Main(string[] args)
         {
-            List<int> seznam = new List<int> { 90, -23, -7, 78 };
-            seznam = BogoSort(seznam);
-            for (int i = 0; i < seznam.Count; i++)
-                Console.WriteLine(seznam[i]);
-        }
-
-        public static List<int> BogoSort(List<int> seznam)
-        {
-            int a = 0;
-
-            for (int i = 1; i < seznam.Count; i++)
-            {
-                if (seznam[i] < seznam[i - 1])
-                {
-                    i = 0;
-                    a++;
-                    seznam = seznam.Shuffle().ToList();
-                }
-            }
-
-            Console.WriteLine(a);
-            Console.WriteLine("seznam je");
-            return seznam;
+            Console.WriteLine("Hello, World!");
         }
     }
 }
