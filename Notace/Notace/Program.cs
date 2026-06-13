53b4838c90346c533a17d2de0db0a497a12d4b07
Ivan66lit <popov.1507i@gmail.com>
2026-06-13 02:27:35 +0200

    Notace vstup a rozdělení znaků ve výrazu

---
+++ Notace/Notace/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Notace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chceš prefix / postfix?");
+            string typ = Console.ReadLine();
+
+            if (typ != "prefix" && typ != "postfix")
+            {
+                Console.WriteLine("Neplatný typ výrazu.");
+                return;
+            }
+
+            Console.WriteLine("Zadej výraz:");
+            string vyraz = Console.ReadLine();
+            string[] znaky = vyraz.Split(' ');
+
+
+            if (typ == "prefix")
+                Console.WriteLine("Větev řešení pro prefix");
+            else
+                Console.WriteLine("Větev řešení pro postfix");
+            foreach (string znak in znaky)
+                Console.WriteLine(znak);
+        }
+    }
+}
